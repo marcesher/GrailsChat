da10b7fd428c22f6b7193d1c13ee39cc47ebed60
Marc <marc.esher@gmail.com>
2012-08-02 20:18:00 -0400

    cleanup

--- grails-app/services/chat/ChatService.groovy
+++ grails-app/services/chat/ChatService.groovy
@@ -10,19 +10,19 @@ class ChatService {
 
     def onRequest = { event ->
         println "Inside onRequest!"
-        try{
+        try {
             AtmosphereRequest req = event.request
             println req
             println req.method
-            if( req.method.equalsIgnoreCase("GET") ){
+            if (req.method.equalsIgnoreCase("GET")) {
                 println 'Suspending'
                 event.suspend()
-            } else if ( req.method.equalsIgnoreCase("POST") ){
+            } else if (req.method.equalsIgnoreCase("POST")) {
                 String stuff = req.reader.readLine().trim()
                 println "Stuff is $stuff"
                 event.broadcaster.broadcast(stuff)
             }
-        } catch( Exception e ){
+        } catch (Exception e) {
             println "ERROR!!!!!"
         }
 
@@ -33,30 +33,30 @@ class ChatService {
         AtmosphereResource r = event.resource
         AtmosphereResponse res = r.response
 
-        try{
-            if( event.isSuspended() ){
+        try {
+            if (event.isSuspended()) {
                 String body = event.message.toString()
                 String author = body.substring(body.indexOf(":") + 2, body.indexOf(",") - 1);
                 String message = body.substring(body.lastIndexOf(":") + 2, body.length() - 2);
-                String data = new Data(author: author, text: message).toString()
-                res.writer.write( data )
+                res.writer.write( createMessage(author, message) )
 
-                switch( r.transport() ){
+                switch (r.transport()) {
                     case TRANSPORT.JSONP:
                     case TRANSPORT.LONG_POLLING:
-                        println "Resuming with data $data"
                         event.resource.resume()
                         break
                     default:
-                        println "Flushing $data"
                         res.writer.flush()
                 }
-            } else if( !event.isResuming() ){
-                event.broadcaster().broadcast( new Data(author: 'Someone', text: 'buh bye').toString() )
+            } else if (!event.isResuming()) {
+                event.broadcaster().broadcast( createMessage('someone', 'buh gye') )
             }
-        } catch( Exception e ){
+        } catch (Exception e) {
             println "ERROR in onStateChange: $e"
         }
+    }
 
+    private String createMessage(String author, String text) {
+        return "{ \"text\" : \"" + text + "\", \"author\" : \"" + author + "\" , \"time\" : " + new Date().time + "}"
     }
 }
